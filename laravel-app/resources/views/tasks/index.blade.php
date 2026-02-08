7d205ba54288bc5c9525b4ce08978a6a79845c0d
Nagitokuta <nagi.tokuta@gmail.com>
2026-02-08 12:55:22 +0900

    ステータスに完了済みがなかったので追加

--- laravel-app/resources/views/tasks/index.blade.php
+++ laravel-app/resources/views/tasks/index.blade.php
@@ -44,6 +44,7 @@
                             @if($task->status === 'pending') bg-gray-100 text-gray-700
                             @elseif($task->status === 'in_progress') bg-blue-100 text-blue-800
                             @elseif($task->status === 'wait_approval') bg-red-100 text-red-800
+                            @elseif($task->status === 'completed') bg-green-100 text-green-800
                             @else bg-green-100 text-green-800
                             @endif">
                         {{ \App\Models\Task::statusLabels()[$task->status] }}
